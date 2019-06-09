df55eb17462a399a4627c020c046114cb409afa2
Rodrigo Rois <rois_r@hotmail.com>
2019-06-09 10:37:48 +0200

    Shared worker - multiple contexts

--- shared-worker/shared-worker.js
+++ shared-worker/shared-worker.js
@@ -1,10 +1,17 @@
+let ports = [], i = 0;
+
 this.addEventListener('connect', e => {
   console.log(e);
 
   let port = e.ports[0];
+  ports.push(port);
   port.start();
 
-  port.addEventListener('message', e => {
-    console.log('Received: ', e.data);
-  });
+  setInterval(() => {
+      i++;
+      ports.forEach((port) =>{
+          port.postMessage('Sending Message ' + i);
+      });
+  }, 1000);
+
 });
